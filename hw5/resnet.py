e8887db94bfd32e30fccba9b85a290ea44cd0093
rahuldesai1 <rahuldesai@berkeley.edu>
2019-08-28 16:39:32 -0700

    Training working, but data preprocessing broken

--- hw5/resnet.py
+++ hw5/resnet.py
@@ -7,42 +7,42 @@ class ResNet():
     def add_residual_block(self, input, block_number, in_channels, out_channels, downsample=False):
         block_number = str(block_number)
         if downsample:
-            skip = tf.nn.max_pool2d(input, [1, 2, 2, 1], [1, 2, 2, 1], "VALID")
+            skip = tf.compat.v1.nn.max_pool2d(input, [1, 2, 2, 1], [1, 2, 2, 1], "VALID")
         else:
             skip = tf.identity(input)
 
         if in_channels != out_channels:
             #perform 1x1 convolution to match output dimensions
-            skip = self.add_convolution(skip, "Wconv2_" + block_number, 1, in_channels, out_channels, "SAME")
+            skip = self.add_convolution(skip, "Wconv2_" + block_number, 1, in_channels, out_channels, "VALID")
         layer = self.add_convolution(input, "Wconv0_" + block_number, 3, in_channels, out_channels, "SAME")
         layer = tf.layers.batch_normalization(layer, training=self.training)
-        layer = tf.nn.relu(layer)
-        layer = self.add_convolution(layer, "Wconv1_" + block_number, 3, in_channels, out_channels, "SAME")
+        layer = tf.compat.v1.nn.relu(layer)
+        layer = self.add_convolution(layer, "Wconv1_" + block_number, 3, out_channels, out_channels, "SAME")
         layer = tf.layers.batch_normalization(layer, training=self.training)
         output = layer + skip
-        return tf.nn.relu(output)
+        return tf.compat.v1.nn.relu(output)
 
     def forward(self, data):
         #7x7 convolution followed by batchnorm, relu, maxpool
         layer = self.add_convolution(data, "Wconv0_0", 7, 3, 64, "SAME")
         layer = tf.layers.batch_normalization(layer, training=self.training)
-        layer = tf.nn.relu(layer)
-        layer = tf.nn.max_pool2d(layer, [1, 3, 3, 1], [1, 2, 2, 1], "VALID")
+        layer = tf.compat.v1.nn.relu(layer)
+        layer = tf.compat.v1.nn.max_pool2d(layer, [1, 3, 3, 1], [1, 2, 2, 1], "VALID")
 
         #8 layers of residual blocks
         layer = self.add_residual_block(layer, 1, 64, 64)
         layer = self.add_residual_block(layer, 2, 64, 64)
 
-        layer = self.add_residual_block(layer, 3, 64, 128, downsample=True)
+        layer = self.add_residual_block(layer, 3, 64, 128)
         layer = self.add_residual_block(layer, 4, 128, 128)
 
-        layer = self.add_residual_block(layer, 5, 128, 256, downsample=True)
+        layer = self.add_residual_block(layer, 5, 128, 256)
         layer = self.add_residual_block(layer, 6, 256, 256)
 
-        layer = self.add_residual_block(layer, 7, 256, 512, downsample=True)
+        layer = self.add_residual_block(layer, 7, 256, 512)
         layer = self.add_residual_block(layer, 8, 512, 512)
 
-        layer = self.add_convolution(layer, "Wconvf", 1, 512, 4, "SAME")
+        layer = self.add_convolution(layer, "Wconvf", 1, 512, 10, "SAME")
         #perform global average pooling on each feature map
         logits = tf.reduce_mean(layer, axis=[1,2])
         return logits
@@ -56,5 +56,6 @@ class ResNet():
                         padding):
         shape = [filter_size, filter_size, input_channels, output_channels]
         Wconv = tf.compat.v1.get_variable(name, shape=shape)
-        layer = tf.nn.conv2d(input, Wconv, padding=padding)
+        bconv = tf.compat.v1.get_variable(name + "b", shape=[output_channels])
+        layer = tf.compat.v1.nn.convolution(input, Wconv, padding=padding, strides=[1, 1, 1, 1]) + bconv
         return layer
--- hw5/rotnet.py
+++ hw5/rotnet.py
@@ -1,6 +1,7 @@
 import yaml
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from tensorflow.python.client import device_lib
 from resnet import ResNet
 from data import Data
 
@@ -16,10 +17,17 @@ class RotNet(object):
         self.model_number = args.model_number
 
         self._populate_model_hyperparameters()
+        self.training_data_obj = Data(self.data_dir,
+                            batch_size=self.batch_size,
+                            height=self.height,
+                            width=self.width
+                            )
         self.build_base_graph()
 
         if args.train:
             self.build_train_graph()
+
+        print(device_lib.list_local_devices())
 
     def _populate_model_hyperparameters(self):
         self.batch_size = self.config["batch_size"]
@@ -28,51 +36,55 @@ class RotNet(object):
         self.learning_rate = self.config["learning_rate"]
         self.height = self.config["image_height"]
         self.width = self.config["image_width"]
+        self.num_epochs = self.config["num_epochs"]
 
     def build_base_graph(self):
-        with tf.device('/cpu:0'):
-            handle = tf.compat.v1.placeholder(tf.string, shape=[])
-            iterator = tf.compat.v1.data.Iterator.from_string_handle(handle, output_types=(tf.float32, tf.int32))
-            X, y = iterator.get_next()
+        self.X = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 32, 32, 3])
+        self.y = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 10])
 
-        with tf.device('/gpu:0'):
+        self.iterator = self.training_data_obj.get_rot_data_iterator(self.X, self.y)
+        data_X, data_y = self.iterator.get_next()
+
+        with tf.device('/cpu:0'):
             model = ResNet()
-            self.logits = model.forward(X)
-            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y))
+            self.logits = model.forward(data_X)
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=data_y))
             self.prob = tf.nn.softmax(self.logits)
             self.predictions = tf.cast(tf.argmax(self.prob, axis=1), tf.float32)
-            actual = tf.cast(tf.argmax(y, axis=1), tf.float32)
+            actual = tf.cast(tf.argmax(data_y, axis=1), tf.float32)
             self.acc = tf.reduce_mean(tf.cast(tf.equal(self.predictions, actual), tf.float32))
 
     def build_train_graph(self):
-        with tf.device('/gpu:0'):
+        with tf.device('/cpu:0'):
             self.opt = tf.compat.v1.train.RMSPropOptimizer(learning_rate=self.learning_rate,
                                             decay=self.weight_decay,
                                             momentum=self.momentum).minimize(self.loss)
             self.saver = tf.compat.v1.train.Saver(max_to_keep=5)
 
     def train(self):
-        data_obj = Data(self.data_dir,
-                    batch_size=self.batch_size,
-                    height=self.height,
-                    width=self.width
-                    )
+        self.sess.run(tf.compat.v1.global_variables_initializer())
 
-        X, y = data_obj.get_training_data()
+        X, y = self.training_data_obj.get_training_data()
+        print(X.shape)
+        print(y.shape)
         X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15 , shuffle=True)
 
-        training_handle = data_obj.get_rot_data_iterator(X_train, y_train)
-        validation_handle = data_obj.get_rot_data_iterator(X_val, y_val)
+        #training_iterator = data_obj.get_rot_data_iterator(X_train, y_train)
+        #validation_iterator = data_obj.get_rot_data_iterator(X_val, y_val)
 
         num_batches = int(len(X_train) / self.batch_size)
         global_step = 0
+        print("[INFO] Starting Training...")
         for epoch in range(self.num_epochs):
+            self.sess.run(self.iterator.initializer, feed_dict = {self.X: X_train, self.y: y_train})
             for batch in range(num_batches):
                 self._update_learning_rate(epoch)
-                _, accuracy = sess.run([self.opt, self.acc], feed_dict={handle: training_handle})
+                _, accuracy = self.sess.run([self.opt, self.acc])
                 global_step += 1
                 print("Epoch: {0}, Batch: {1} ==> Accuracy: {2}".format(epoch, batch, accuracy))
-            loss, accuracy = sess.run([self.loss, self.acc], feed_dict={validation_handle})
+
+            self.sess.run(self.iterator.initializer, feed_dict = {self.X: X_val, self.y: y_val})
+            loss, accuracy = self.sess.run([self.loss, self.acc])
             print("(Validation) Epoch: {0} ===> Accuracy: {2}".format(epoch, accuracy))
             self.save_checkpoint(global_step, epoch)
 
